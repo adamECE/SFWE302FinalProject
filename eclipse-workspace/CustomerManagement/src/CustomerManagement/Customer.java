a3074739738cfe8e47a2d2f3696c7d364ee7f518
adamECE <85218358+adamECE@users.noreply.github.com>
2023-05-01 16:45:53 -0700

    Customer can write to csv

--- eclipse-workspace/CustomerManagement/src/CustomerManagement/Customer.java
+++ eclipse-workspace/CustomerManagement/src/CustomerManagement/Customer.java
@@ -1,6 +1,11 @@
 package CustomerManagement;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import CheckoutSystem.Invoice;
 public class Customer {
@@ -24,7 +29,15 @@ public class Customer {
 		this.PolicyHolder = PolHol;
 	}
 	
-	public void createAccount(Scanner scan) {
+	public static void createAccount(String n, String date, String Add, Integer phone, String InsCom, Integer groupNum, String PolHol) throws IOException {
+		Customer newCustomer = new User(n, date, Add, phone, InsCom, groupNum, PolHol);
 		
+		BufferedWriter writer = new BufferedWriter(new FileWriter("customerDB.csv", true));
+		try {
+			writer.append(n + "," + date + "," + Add + "," + phone "," + phInsComone "," + groupNum "," + PolHol);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		writer.close();
 	}
 }
